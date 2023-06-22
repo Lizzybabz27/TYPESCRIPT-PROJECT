a8735a372bc5d59d20844f1273bc56f1f48bfe88
OMIGIE OSARHIEMEN ELIZABETH <omigieelizabeth2018@gmail.com>
2023-06-22 14:17:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+let todos = [];
+let loop = true;
+while (loop) {
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: "What do you want to add in your Todo?"
+        },
+        {
+            type: "confirm",
+            name: "addMore",
+            message: "Do you want to add more items to your Todo? "
+        },
+    ]);
+    const { TODO, addMore } = answers;
+    loop = addMore;
+    if (TODO) {
+        todos.push(TODO);
+    }
+    else {
+        console.log("kindly add valid input");
+    }
+}
+if (todos.length > 0) {
+    console.log("Your Todo List is: ");
+    todos.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("No Todos Found");
+}
+/*
+
+
+
+*/ 
